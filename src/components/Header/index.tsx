326475934b9cd03130b054643890092afc70f70b
bernardojru <bernardogomes860@gmail.com>
2022-11-28 12:53:43 +0100

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import { HeaderContainer } from "./styles";
+
+import Image from "next/image";
+import Link from "next/link";
+import logoImg from "../../assets/logo.svg";
+import { Cart } from "../Cart";
+import { useRouter } from "next/router";
+
+export function Header() {
+  const { pathname } = useRouter();
+
+  const showCartButton = pathname !== "/success";
+
+  return (
+    <HeaderContainer>
+      <Link href="/">
+        <Image src={logoImg} alt="" />
+      </Link>
+      {showCartButton && <Cart />}
+    </HeaderContainer>
+  );
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,41 @@
+import { globalCss } from "@stitches/react";
+
+export const globalStyles = globalCss({
+  "*": {
+    margin: 0,
+    padding: 0,
+
+    "::-webkit-scrollbar": {
+      width: 6,
+    },
+
+    "::-webkit-scrollbar-track": {
+      background: "$gray900",
+    },
+
+    "::-webkit-scrollbar-thumb": {
+      background: "$green500",
+      borderRadius: 10,
+    },
+  },
+
+  body: {
+    backgroundColor: "$gray900",
+    color: "$gray100",
+    "-webkit-font-smoothing": "antialiased",
+    marginBottom: "5rem",
+  },
+
+  "body, input, textarea, button": {
+    fontFamily: "Roboto",
+    fontWeight: 400,
+  },
+
+  a: {
+    color: "inherit",
+  },
+
+  button: {
+    cursor: "pointer",
+  },
+});
